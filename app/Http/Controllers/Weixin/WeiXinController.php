41b270e51f0bda053149d481fd7743bbcc7a54e2
LiShiYv <qq15354304250@163.com>
2019-02-18 15:23:01 +0800

    2.18.6.1

--- app/Http/Controllers/Weixin/WeiXinController.php
+++ app/Http/Controllers/Weixin/WeiXinController.php
@@ -26,46 +26,7 @@ class WeiXinController extends Controller
         $data = file_get_contents("php://input");
         $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
         file_put_contents('logs/wx_event.log',$log_str,FILE_APPEND);
-        //解析xml
-        exit;
-        $xml= simplexml_load_string($data);  //将 xml字符串 转换成对象
-        $event =$xml->Event;    //事件类型
-        //var_dump($xml);echo '<hr>';
-        if($event=='subscribe') {
-            $openid = $xml->FromUserName;               //用户openid
-            $sub_time = $xml->CreateTime;               //扫码关注时间
-
-
-            echo 'openid: ' . $openid;
-            echo '</br>';
-            echo '$sub_time: ' . $sub_time;
-
-            //获取用户信息
-            $user_info = $this->getUserInfo($openid);
-            echo '<pre>';
-            print_r($user_info);
-            echo '</pre>';
-
-            //保存用户信息
-            $u = WeixinUser::where(['openid' => $openid])->first();
-            //var_dump($u);die;
-            if ($u) {       //用户不存在
-                echo '用户已存在';
-            } else {
-                $user_data = [
-                    'openid' => $openid,
-                    'add_time' => time(),
-                    'nickname' => $user_info['nickname'],
-                    'sex' => $user_info['sex'],
-                    'headimgurl' => $user_info['headimgurl'],
-                    'subscribe_time' => $sub_time,
-                ];
-
-                $id = WeixinUser::insertGetId($user_data);      //保存用户信息
-                var_dump($id);
-            }
-        }
-
+       
     }
 
 
