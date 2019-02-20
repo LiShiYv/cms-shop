24d87619c7ce82f0d80164bc2ce178908678d659
LiShiYv <qq15354304250@163.com>
2019-02-20 13:41:18 +0800

    2.20.1.11

--- app/Http/Controllers/Weixin/WeiXinController.php
+++ app/Http/Controllers/Weixin/WeiXinController.php
@@ -290,26 +290,26 @@ public function dlVideo($media_id){
 public function wxMenuType()
 {
     $url = 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token=' . $this->getWXAccessToken();
-   // echo $url;echo '</br>';
+    //echo $url;echo '</br>';
 
     //2 请求微信接口
     $client = new GuzzleHttp\Client(['base_uri' => $url]);
     $data = [
-        "filter"=>[
+        "filter" => [
 
-    "is_to_all"=>true,
-      "tag_id"=>2
-   ],
-   "text"=>[
-    "content"=>"CONTENT"
-   ],
-    "msgtype"=>"text"
+                "is_to_all" => true,
+                "tag_id" => 2     // view类型 跳转指定 URL
+            ],
+            "text" => [
+                "content"=>"群发"
+            ],
+
+        "msgtype" => "text"
 
     ];
-    var_dump($data);
-      //处理中文编码
-    $r = $client->request('POST', $url,  [ 'body' => json_encode($data,JSON_UNESCAPED_UNICODE)
-
+    $body = json_encode($data,JSON_UNESCAPED_UNICODE);      //处理中文编码
+    $r = $client->request('POST', $url, [
+        'body' => $body
     ]);
 
     // 3 解析微信接口返回信息
