44904322d1054b654bff720436b0818b43fef52d
LiShiYv <qq15354304250@163.com>
2019-02-23 11:23:48 +0800

    2.23.18

--- resources/views/weixin/wxservice.blade.php
+++ resources/views/weixin/wxservice.blade.php
@@ -10,7 +10,7 @@
         <h1>客服</h1>
         {{csrf_field()}}
 
-        <textarea type="text" name="weixin"  cols="60" rows="20">聊天记录：</textarea>
+        聊天记录：<textarea type="text" name="weixin"  cols="60" rows="20"></textarea>
         请输入：<input type="text" name="weixin" id="weixin" >
         <button type="submit" class="btn btn-default" id="text">发送</button>
     </form>
