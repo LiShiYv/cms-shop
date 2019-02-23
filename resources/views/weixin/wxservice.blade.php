7285076313b369c78982e5cd528af0e2e020eeeb
LiShiYv <qq15354304250@163.com>
2019-02-23 13:30:05 +0800

    2.23.28

--- resources/views/weixin/wxservice.blade.php
+++ resources/views/weixin/wxservice.blade.php
@@ -1,19 +1,25 @@
 <html lang="en">
     <head>
+        <h1>客服</h1>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
+        <script src="js/jquery-1.12.4.min.js"></script>
+        {{csrf_field()}}
     </head>
     <body>
-    <form>
-        <h1>客服</h1>
-        {{csrf_field()}}
-
-        聊天记录：<textarea type="text" name="weixin"  cols="60" rows="20"></textarea>
-        请输入：<input type="text" name="weixin" id="weixin" >
-        <button type="submit" class="btn btn-default" id="text">发送</button>
-    </form>
+         <form>
+            <table>
+               <tr>
+                   <td>聊天记录：<textarea type="text" name="weixin"  cols="60" rows="20"></textarea></td>
+               </tr>
+                <tr>
+                    <td>请输入：<input type="text" name="weixin" id="weixin" ></td>
+                </tr>
+                <button type="submit" class="btn btn-default" id="text">发送</button>
+            </table>
+         </form>
     </body>
     </html>
 
