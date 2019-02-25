a3f501d384e95e2619c073c3ddc23e70a29733f8
LiShiYv <qq15354304250@163.com>
2019-02-25 16:51:35 +0800

    2.25.3

--- resources/views/weixin/wxservice.blade.php
+++ resources/views/weixin/wxservice.blade.php
@@ -1,7 +1,7 @@
 <html lang="en">
     <head>
         <h1>客服</h1>
-        <h2>正在和“{{$user_info['nickname']}}”聊天中</h2>
+        <h2>正在和“<font color="#778899">{{$user_info['nickname']}}</font>”聊天中</h2>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
