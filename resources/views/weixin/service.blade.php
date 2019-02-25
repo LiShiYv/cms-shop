cfdf08a20bd622ecc7467a3b4ac149a85ac634cd
LiShiYv <qq15354304250@163.com>
2019-02-25 16:53:15 +0800

    2.25.3

--- resources/views/weixin/service.blade.php
+++ resources/views/weixin/service.blade.php
@@ -5,6 +5,7 @@
 <html lang="en">
 <head>
     <h1>客服</h1>
+    <h2>正在和“<font color="red">{{$nickname}}</font>”聊天中</h2>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -16,7 +17,7 @@
     <table>
         <tr>
             <td>聊天记录</td>
-            <td><div style="width:00px;height:700px;overflow:auto;border: solid black 1px" id="content"></div></td>
+            <td><div style="width:600px;height:700px;overflow:auto;border: solid black 1px" id="content"></div></td>
         </tr>
         <input type="hidden" class="openid" value="{{$openid}}">
         <input type="hidden" class="nickname" value="{{$nickname}}">
