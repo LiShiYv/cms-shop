d40325c471c75728151c93c64791264e4870e8ab
LiShiYv <qq15354304250@163.com>
2019-02-26 16:23:04 +0800

    ZF

--- app/Http/Controllers/Weixin/PayController.php
+++ app/Http/Controllers/Weixin/PayController.php
@@ -57,9 +57,9 @@ class PayController extends Controller
 //		echo 'result_code: '.$data->result_code;echo '<br>';
 //		echo 'prepay_id: '.$data->prepay_id;echo '<br>';
 //		echo 'trade_type: '.$data->trade_type;echo '<br>';
-       // echo 'code_url: '.$data->code_url;echo '<br>';
+       echo 'code_url: '.$data->code_url;echo '<br>';
 //        die;
-        echo '<pre>';print_r($data);echo '</pre>';
+        //echo '<pre>';print_r($data);echo '</pre>';
 
         //将 code_url 返回给前端，前端生成 支付二维码
         // return code_url;
